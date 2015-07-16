4e5911055aefe9460f3247a364f6cf588c2ce6a9
GrandCircusPaul <paul@grandcircus.com>
2015-07-16 11:05:32 -0400

    Created MVC Template Project
    Default MVC Template Project created, with authorization, without
    publish to Azure

---
+++ tMVCDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tMVCDemo.Startup))]
+namespace tMVCDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
